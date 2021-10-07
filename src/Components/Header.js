0011b1a80457758051c394f3dca50925f6070e58
abi-hong <68041042+abi-hong@users.noreply.github.com>
2021-10-07 18:36:16 +0900

    change header css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Header(props) {
   return (
@@ -17,8 +17,16 @@ function Header(props) {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link><Link to="/introduce">소개</Link></Nav.Link>
-            <Nav.Link><Link to="/project">프로젝트</Link></Nav.Link>
+            <Nav.Link>
+              <Link className="header" to="/introduce">
+                소개
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className="header" to="/project">
+                프로젝트
+              </Link>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
