20ad14c42e135f6b14e3a2ba5a74a85c4debf32b
ilmerry <milkiss0414@naver.com>
2021-10-07 17:11:09 +0900

    navlink 재수정

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,8 +16,8 @@ function Header(props) {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link to="/introduce">소개</Nav.Link>
-            <Nav.Link to="/project">프로젝트</Nav.Link>
+            <Nav.Link to="/#/introduce">소개</Nav.Link>
+            <Nav.Link to="/#/project">프로젝트</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
