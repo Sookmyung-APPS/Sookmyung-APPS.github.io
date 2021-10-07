faf87edc92081159d1670f24d8e10e0919c4c959
뽁 <o0o1910419@sookmyung.ac.kr>
2021-10-07 19:45:53 +0900

    페이지 이동 문제 해결

--- src/Components/Download.js
+++ src/Components/Download.js
@@ -3,22 +3,22 @@ import React from "react";
 const array = [
     {
         name: "스팟지도웹서비스",
-        DownloadLink1: "#",
-        DownloadLink2: "#",
-        DownloadLink3: "#",
+        DownloadLink1: "#/project/스팟지도웹서비스",
+        DownloadLink2: "#/project/스팟지도웹서비스",
+        DownloadLink3: "#/project/스팟지도웹서비스",
         GithubLink: "https://github.com/Sookmyung-APPS/Captured-Moment"    
     },
     {
         name: "DeliveryRun",
-        DownloadLink1: "#",
-        DownloadLink2: "#",
+        DownloadLink1: "#/project/DeliveryRun",
+        DownloadLink2: "#/project/DeliveryRun",
         DownloadLink3: "https://drive.google.com/file/d/1CJ52YiNUBHpTnN-Lmo17MSqHCfrYMJoD/view?usp=sharing",
         GithubLink: "https://github.com/Sookmyung-APPS/DeliveryRun"    
     },
     {
         name: "일력",
-        DownloadLink1: "#",
-        DownloadLink2: "#",
+        DownloadLink1: "#/project/일력",
+        DownloadLink2: "#/project/일력",
         DownloadLink3: "https://drive.google.com/file/d/1Q-0t7_rrp81bLI3ArUuTMgGWQOK8g71r/view?usp=sharing",
         GithubLink: "https://github.com/Sookmyung-APPS/Dia-lendar"    
     },
@@ -26,21 +26,21 @@ const array = [
         name: "LightAway",
         DownloadLink1: "https://drive.google.com/file/d/1mj1ktK3BiHZCNv3uTAlZdF3CxcqafmaR/view?usp=sharing",
         DownloadLink2: "https://drive.google.com/file/d/1qTNxJePLBfirQA2CxoBvDZ1pu-7DYv_Y/view?usp=sharing",
-        DownloadLink3: "#",
+        DownloadLink3: "#/project/LightAway",
         GithubLink: "https://github.com/Sookmyung-APPS/LightAway"    
     },
     {
         name: "SoundOfFairies",
-        DownloadLink1: "#",
-        DownloadLink2: "#",
+        DownloadLink1: "#/project/SoundOfFairies",
+        DownloadLink2: "#/project/SoundOfFairies",
         DownloadLink3: "https://drive.google.com/file/d/1ETk4Bk264G4NTTukfPhsnjkq2Faubtbt/view?usp=sharing",
         GithubLink: "https://github.com/Sookmyung-APPS/SoundOfFairies"    
     },
     {
         name: "Comsongz",
-        DownloadLink1: "#",
-        DownloadLink2: "#",
-        DownloadLink3: "#",
+        DownloadLink1: "#/project/Comsongz",
+        DownloadLink2: "#/project/Comsongz",
+        DownloadLink3: "#/project/Comsongz",
         GithubLink: "https://github.com/Sookmyung-APPS/Comsongz"    
     },
     {
@@ -61,57 +61,57 @@ const array = [
     {   //김다연
         name: "김다연",
         DownloadLink1: "https://drive.google.com/file/d/1K-R09VSp8ssAEaY0n72uw7kAgO4ylSuP/view?usp=sharing",
-        DownloadLink2: "#",
-        DownloadLink3: "#",
+        DownloadLink2: "#/project/김다연",
+        DownloadLink3: "#/project/김다연",
         GithubLink: "https://github.com/Sookmyung-APPS/TERRAFORMING"
     },
     {   //김진영
         name: "김진영",
-        DownloadLink1: "#",
+        DownloadLink1: "#/project/김진영",
         DownloadLink2: "https://drive.google.com/file/d/1F2OrFcJeqqiWIAaAmuL711jp30GcRLzh/view?usp=sharing",
-        DownloadLink3: "#",
+        DownloadLink3: "#/project/김진영",
         GithubLink: "https://github.com/Sookmyung-APPS/Make-My-Wish-Come-True"
     },
     {   //박해지
         name: "박해지",
-        DownloadLink1: "#",
+        DownloadLink1: "#/project/박해지",
         DownloadLink2: "https://drive.google.com/file/d/1jq1tFL5M-F8wJBISxmXgY0Qvq1uCjKzJ/view?usp=sharing",
-        DownloadLink3: "#",
+        DownloadLink3: "#/project/박해지",
         GithubLink: "https://github.com/Sookmyung-APPS/RoomEscapeGame"
     },
     {   //이가을
         name: "이가을",
-        DownloadLink1: "#",
+        DownloadLink1: "#/project/이가을",
         DownloadLink2: "https://drive.google.com/file/d/1NRo2BKj4raNXm_JiCq1f-oiUcaW_lsFl/view?usp=sharing",
-        DownloadLink3: "#",
+        DownloadLink3: "#/project/이가을",
         GithubLink: "https://github.com/Sookmyung-APPS/SNOWJUMP"
     },
     {   //장나리
         name: "장나리",
         DownloadLink1: "https://drive.google.com/file/d/1b9qbpwHg6J8nC_aKBz4t7WgqJZdWauVw/view?usp=sharing",
         DownloadLink2: "https://drive.google.com/file/d/1b23IXQ023QW9Njn39opxA67ZrRo7TO9v/view?usp=sharing",
-        DownloadLink3: "#",
+        DownloadLink3: "#/project/장나리",
         GithubLink: "https://github.com/Sookmyung-APPS/SNOWRUN"
     },
     {   //정민영
         name: "정민영",
         DownloadLink1: "https://drive.google.com/file/d/15teX9O5Zfo9MrRpE3OxJr7QcF8uUC1tB/view?usp=sharing",
         DownloadLink2: "https://drive.google.com/file/d/1JXHYKX225fo9zcREasEIvSTOVcpoBfod/view?usp=sharing",
-        DownloadLink3: "#",
+        DownloadLink3: "#/project/정민영",
         GithubLink: "https://github.com/Sookmyung-APPS/2DSnowflakeGame"
     },
     {   //정채영
         name: "정채영",
-        DownloadLink1: "#",
+        DownloadLink1: "#/project/정채영",
         DownloadLink2: "https://drive.google.com/file/d/1eIsWPfudbMSDYrwcPgKu0tT5m1KITvfj/view?usp=sharing",
-        DownloadLink3: "#",
+        DownloadLink3: "#/project/정채영",
         GithubLink: "https://github.com/Sookmyung-APPS/RollingBall"
     },
     {   //홍희수
         name: "홍희수",
         DownloadLink1: "https://drive.google.com/file/d/1M-WuAsOH1HxPO_cpUoOa9I8Mt2T-RoPf/view?usp=sharing",
-        DownloadLink2: "#",
-        DownloadLink3: "#",
+        DownloadLink2: "#/project/홍희수",
+        DownloadLink3: "#/project/홍희수",
         GithubLink: "https://github.com/Sookmyung-APPS/RollNoonSong"
     },
 ];
@@ -138,9 +138,9 @@ function Download ( { name }) {
 
     return (
         <div style={dev}>
-            <a href={array[index].DownloadLink3} rel="noreferrer" target="_blank"><img style={devIMG} alt="" src={require("../Assets/downloadApk.png").default} /></a>
-            <a href={array[index].DownloadLink1} rel="noreferrer" target="_blank"><img style={devIMG} alt="" src={require("../Assets/downloadMac.png").default} /></a>
-            <a href={array[index].DownloadLink2} rel="noreferrer" target="_blank"><img style={devIMG} alt="" src={require("../Assets/downloadWindow.png").default} /></a>
+            <a href={array[index].DownloadLink3} rel="noreferrer"><img style={devIMG} alt="" src={require("../Assets/downloadApk.png").default} /></a>
+            <a href={array[index].DownloadLink1} rel="noreferrer"><img style={devIMG} alt="" src={require("../Assets/downloadMac.png").default} /></a>
+            <a href={array[index].DownloadLink2} rel="noreferrer"><img style={devIMG} alt="" src={require("../Assets/downloadWindow.png").default} /></a>
             <a href={array[index].GithubLink} rel="noreferrer" target="_blank"><img style={devIMG} alt="" src={require("../Assets/github.png").default} /></a>
         </div>
     )
